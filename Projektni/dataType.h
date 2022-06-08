2f70c1b730e716f24afcfd85bb0c9f9f55b200cf
lcrljic <105847406+lcrljic@users.noreply.github.com>
2022-06-08 09:36:56 +0200

    Add files via upload

---
+++ Projektni/dataType.h
@@ -0,0 +1,12 @@
+#ifndef DATATYPE_H
+#define DATATYPE_H
+
+typedef struct kontakt {
+	int id;
+	char ime[25];
+	char prezime[25];
+	char adresa[50];
+	char brojMobitela[11];
+}KONTAKT;
+
+#endif
---
+++ Projektni/main.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include "functions.h"
+#include "dataType.h"
+
+int main(void) {
+
+	int odgovor = 1;
+
+	char* datoteka = "kontakti.bin";
+
+	while (odgovor) {
+		odgovor = izbornik(datoteka);
+	}
+
+	printf("Kraj programa");
+
+	return 0;
+}
